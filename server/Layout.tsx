117da7596f33a4699edfcf38c33fca26542216d3
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-03 17:09:40 +1200

    add hyperscript

--- server/Layout.tsx
+++ server/Layout.tsx
@@ -4,9 +4,9 @@ export function Layout({ children }: Attributes) {
   return `<!DOCTYPE >
     <html>
       <head>
-
-<script src="https://unpkg.com/htmx.org@1.9.4" integrity="sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/htmx.org@1.9.4" integrity="sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/htmx.org/dist/ext/ws.js"></script>
+        <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
       </head>
       <body>
         <div hx-ext="ws" ws-connect="ws://localhost:8080">
