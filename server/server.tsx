3b55242c2e0b736b5848d27301d8df51457e1191
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-03 16:59:27 +1200

    implement broadcasting

--- server/server.tsx
+++ server/server.tsx
@@ -1,40 +1,52 @@
 import express from 'express'
-import * as elements from 'typed-html'
-import { WebSocketServer } from 'ws'
-import { Layout } from './Layout.tsx'
+import WebSocket, { WebSocketServer } from 'ws'
 
 const app = express()
-export default app
+const wss = new WebSocketServer({ port: 8080 })
 
-const wss = new WebSocketServer({ port: 8080, noServer: false })
+// Broadcast function to send messages to all connected clients
+function broadcast(data, options) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data, options)
+    }
+  })
+}
 
 wss.on('connection', (ws) => {
-  ws.on('error', console.error)
+  console.log('connected')
 
-  ws.on('message', (data) => {
+  ws.on('message', (data, isBinary) => {
     const message = JSON.parse(data.toString())
     console.log('received: %s', message.chat_message)
+    const response = `<div id="notifications" hx-swap-oob="afterend">
+<p>${message.chat_message}</p>
+</div>`
+    broadcast(response, { binary: isBinary })
   })
-
-  setTimeout(() => {
-    ws.send(
-      `<div id="notifications" hx-swap-oob="true">A message from the server</div>`
-    )
-  }, 10000)
 })
 
 app.get('/', (req, res) => {
-  res.send(
-    <Layout>
-      <div hx-ext="ws" ws-connect="ws://localhost:8080">
-        <div id="notifications" hx-swap-oob="beforeend">
-          New message received
+  res.send(`
+    <html>
+      <head>
+
+<script src="https://unpkg.com/htmx.org@1.9.4" integrity="sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/htmx.org/dist/ext/ws.js"></script>
+      </head>
+      <body>
+        <div hx-ext="ws" ws-connect="ws://localhost:8080">
+          <form ws-send>
+            <input name="chat_message" value="" />
+            <button type="submit">Send</button>
+          </form>
+          <div id="notifications"></div>
         </div>
-        <form ws-send>
-          <input name="chat_message" value="hello" />
-          <button type="submit">Send</button>
-        </form>
-      </div>
-    </Layout>
-  )
+      </body>
+    </html>
+  `)
+})
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000')
 })
