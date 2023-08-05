e724ba0e6a837046d9641fef5761afd7f511892d
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-06 07:40:42 +1200

    improve ux

--- server/components/PublishedQuestion.tsx
+++ server/components/PublishedQuestion.tsx
@@ -13,26 +13,30 @@ export interface Props {
 
 export function PublishedQuestion({ question, options }: Props) {
   return (
-    <div class="grid place-items-center">
+    <form
+      id="publishedQuestion"
+      class="flex flex-col gap-2"
+      hx-ext="ws"
+      ws-send
+      ws-connect="/poll"
+    >
       <h1 class="text-2xl">{question}</h1>
-      <form class="flex flex-col gap-2" hx-ext="ws" ws-send ws-connect="/poll">
-        {options
-          .filter((option) => option.name)
-          .map((option, i) => (
-            <div class="flex gap-2">
-              <input
-                type="radio"
-                id={i.toString()}
-                name="option"
-                value={i.toString()}
-              />
-              <label for={i.toString()}>{option.name}</label>
-            </div>
-          ))}
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Vote
-        </button>
-      </form>
-    </div>
+      {options
+        .filter((option) => option.name)
+        .map((option, i) => (
+          <div class="flex gap-2">
+            <input
+              type="radio"
+              id={i.toString()}
+              name="option"
+              value={i.toString()}
+            />
+            <label for={i.toString()}>{option.name}</label>
+          </div>
+        ))}
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Vote
+      </button>
+    </form>
   )
 }
