0db9cd67acc88d2c8e8325a92e265a011b6dfa0f
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-06 13:58:06 +1200

    refactor

--- server/websockets/poll.tsx
+++ server/websockets/poll.tsx
@@ -6,17 +6,18 @@ import { broadcast } from '../broadcast.tsx'
 import Thanks from '../components/Thanks.tsx'
 
 const pollWss = new WebSocketServer({ noServer: true })
-export default pollWss
 
 pollWss.on('connection', (ws, req) => {
   // Handle poll connections
   console.log('poll connected')
   ws.on('message', (data) => {
     const message = JSON.parse(data.toString())
-    console.log(message)
+    // console.log(message)
     const userId = cookie.parse(req.headers.cookie || '').userId
-    // console.log(`${userId} - ${message.option}`)
+    console.log(`${userId} - ${message.option}`)
 
     broadcast(ws, <Thanks />)
   })
 })
+
+export default pollWss
